7384826326750c80311a788a5e34eebb190186f8
noahcovey <nmcovey1@gmail.com>
2018-10-19 21:50:55 -0400

    small sizing changes

--- page_script_all.js
+++ page_script_all.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     console.log("Starting JS All");
 
     $(window).scroll(function() {
-        if ($(this).scrollTop() > $(".nav").height()) {
+        if ($(this).scrollTop() > $(".nav").height()/2) {
             $(".nav").addClass("nav-scrolled");
         } else {
             $(".nav").removeClass("nav-scrolled");
