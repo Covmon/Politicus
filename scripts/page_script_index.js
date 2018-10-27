8fa5cd75907a2e0fc4d81e3aa1693b24a344e64a
noahcovey <nmcovey1@gmail.com>
2018-10-27 19:45:56 -0400

    getting ready

--- scripts/page_script_index.js
+++ scripts/page_script_index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     console.log("Starting JS Index Page");
     
-    getElections(["State Representative", "State Senator", "0", "U.S. Representative", "U.S. Senator"], 3, false);
+    getElections(["State Representative", "State Senator", "0", "U.S. Representative", "U.S. Senator"], 6, false);
 
 });
