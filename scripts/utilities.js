6a2ed02dea9f77ea10c3c4d0fe9692ecacef27b2
noahcovey <nmcovey1@gmail.com>
2018-11-04 15:59:02 -0500

    added vermont

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -3,7 +3,7 @@ var currentOverallData = {};
 var allOverallData = [];
 let availableStates = ["CO", "IA", "MO", "NY", "SC", "TN", "KS", "GA", "UT", "MI", "ID", "MN", "MA"];
 //missing from house states: AK, DE, MT, ND, SD, VT, WY
-let availableStatesNoStateLegislatures = ["AL", "AR", "AZ", "CA", "CT", "FL", "HI", "IL", "IN", "KY", "LA", "MD", "ME", "MS", "NC", "NE", "NH", "NJ", "NM", "NV", "OH", "OK", "OR", "PA", "RI", "TX", "VA", "WA", "WI", "WV"];
+let availableStatesNoStateLegislatures = ["AL", "AR", "AZ", "CA", "CT", "FL", "HI", "IL", "IN", "KY", "LA", "MD", "ME", "MS", "NC", "NE", "NH", "NJ", "NM", "NV", "OH", "OK", "OR", "PA", "RI", "TX", "VA", "VT", "WA", "WI", "WV"];
 var currentStates = availableStates;
 var currentAllMatchups = [];
 var currentDistrictsNoElection = [];
