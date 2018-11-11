c266e56a2b3c8cf8538d73e7b23e8867fad51eb5
noahcovey <nmcovey1@gmail.com>
2018-11-11 14:08:52 -0500

    moved liveblog

--- scripts/page_script_news.js
+++ scripts/page_script_news.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     console.log("JS News Script Loaded");
 
+    getNewsArticle("2018_liveblog");
     getNewsArticle("GA_State_Representative");
     getNewsArticle("GA_State_Senator");
     getNewsArticle("CO_State_Representative");
