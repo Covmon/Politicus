d4830c45810338a5f2aa2d7d5c093cddbfa7fc80
noahcovey <nmcovey1@gmail.com>
2019-01-04 18:18:14 -0500

    added news article for analysis

--- scripts/page_script_news.js
+++ scripts/page_script_news.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     console.log("JS News Script Loaded");
 
+    getNewsArticle("analysis");
     getNewsArticle("2018_liveblog");
     getNewsArticle("GA_State_Representative");
     getNewsArticle("GA_State_Senator");
